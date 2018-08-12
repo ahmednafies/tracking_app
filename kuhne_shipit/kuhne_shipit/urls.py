0d31a30302b146b86e396280beb07d4ad88fef74
ahmed <ahmed.nafies@gmail.com>
2018-08-12 15:17:30 +0300

    static urls added for show purposes

--- kuhne_shipit/kuhne_shipit/urls.py
+++ kuhne_shipit/kuhne_shipit/urls.py
@@ -7,7 +7,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     url(r'', include('auth.urls')),
     url(r'', include('shipments.urls')),
-    url(r'^api/v1/', include('api.urls', namespace="api-v1")),
+    url(r'^api/v1/', include('api.urls', namespace="api-v1"), name="api-v1"),
     url(r'^api/v2/', include('api_v2.urls', namespace="api-v2")),
     url(r'^auth/login/$', obtain_jwt_token, name='api-login'),
     url(r'^admin/', admin.site.urls),
