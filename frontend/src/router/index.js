3d27ddf1d824b7c723a4e6f9d6f685866e645791
ahmed <ahmed.nafies@gmail.com>
2018-08-19 17:16:28 +0300

    proper routes are added to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import shipments from '@/components/shipments'
+import companies from '@/components/companies'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'shipments',
+      component: shipments
+    },
+    {
+      path: '/companies',
+      name: 'companies',
+      component: companies
     }
   ]
 })
