e2081938d2f109415d8753ef5b1a1a5fee256052
ahmed <ahmed.nafies@gmail.com>
2018-08-20 12:22:54 +0300

    token url is modified

--- kuhne_shipit/kuhne_shipit/urls.py
+++ kuhne_shipit/kuhne_shipit/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'', include('shipments.urls')),
     url(r'^api/v1/', include('api.urls', namespace="api-v1"), name="api-v1"),
     url(r'^api/v2/', include('api_v2.urls', namespace="api-v2")),
-    url(r'^auth/login/$', obtain_jwt_token, name='api-login'),
+    url(r'^token$', obtain_jwt_token, name='api-login'),
     url(r'^admin/', admin.site.urls),
 ]
